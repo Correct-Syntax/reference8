4f2d82b86c994a648bce281a6c5552f83b715137
Noah Rahm <correctsyntax@yahoo.com>
2024-01-26 13:18:44 -0600

    Commit changes

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,8 @@ import IconChevronRight from './components/icons/IconChevronRight.vue';
   <header class="absolute top-0 left-0 bg-dark-900 py-2 px-5 w-full">
     <nav class="relative flex flex-row items-center">
       <RouterLink to="/"><img alt="Ref logo" class="w-5 h-5" src="@/assets/logo-icon.svg" /></RouterLink>
-      <IconChevronRight class="fill-neutral-400 w-2 h-2 mx-1.5" />
-      <div class="text-neutral-300 text-sm">Reference Image</div>
+      <!-- <IconChevronRight class="fill-neutral-400 w-2 h-2 mx-1.5" /> -->
+      <div class="text-neutral-300 text-xs mx-1.5">Reference 8</div>
     </nav>
   </header>
   <RouterView />
