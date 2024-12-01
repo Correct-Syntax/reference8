dda9769063c9801384f6f053ef89305f0c526c1d
Noah Rahm <correctsyntax@yahoo.com>
2024-12-01 12:16:00 -0600

    Improve DropDownMenu component

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -1,21 +1,15 @@
 "use client";
 
-import { ViewGridIcon } from "@radix-ui/react-icons";
-
-function getIcon() {
-    return null;
-}
-
-export default function DropDownMenu({items}) {
-    const listItems = items.map(item => 
-        <li key={item.id} className="flex items-center space-x-2 font-semibold cursor-pointer text-gray">
-            <ViewGridIcon className="w-5 h-5"></ViewGridIcon>
+export default function DropDownMenu({ items, selected, onClick }) {
+    const listItems = items.map(item =>
+        <li onClick={() => onClick(item.id)} key={item.id} className={`px-3 py-2 flex items-center space-x-2 font-bold cursor-pointer rounded-lg transition-colors duration-300 ease-in-out hover:bg-accent/10 ${selected == item.id ? "text-accent bg-accent/10" : "text-gray hover:text-accent"}`}>
+            <item.icon className="w-5 h-5" />
             <span>{item.name}</span>
         </li>
     );
     return (
-        <div className="px-4 py-5 w-44 rounded-lg bg-foreground">
-            <ul className="space-y-3">{listItems}</ul>
+        <div className="absolute right-1 top-10 p-2 w-52 text-[11pt] rounded-lg bg-foreground">
+            <ul className="space-y-1">{listItems}</ul>
         </div>
     );
 }
