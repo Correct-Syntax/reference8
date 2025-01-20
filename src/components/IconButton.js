da307b8eab8d74bed7cf5a2278f2e8a17a9cb861
Noah Rahm <correctsyntax@yahoo.com>
2025-01-19 18:37:49 -0600

    .

--- src/components/IconButton.js
+++
@@ -1,8 +0,0 @@
-
-export default function IconButton({ children, disabled, onClick }) {
-    return (
-        <button onClick={onClick} disabled={disabled} className={`p-0.5 transition-colors duration-300 ease-in-out  ${disabled ? "text-gray/50" : "text-gray hover:text-white"}`}>
-            {children}
-        </button>
-    );
-}
